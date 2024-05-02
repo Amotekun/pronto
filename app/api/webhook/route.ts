b34ba777e5d8b28e6c9bfa51416296cee2e9a3af
Elijah Ajayi <elijahajayi444@gmail.com>
2024-05-02 15:01:07 -0700

    stripe

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -1,12 +1,11 @@
 import db from "@/lib/db";
 import { stripe } from "@/lib/stripe";
-import { headers } from "next/headers";
 import { NextResponse } from "next/server";
 import Stripe from "stripe";
 
 export async function POST(req: Request) {
     const body = await req.text();
-    const signature = headers().get("Stripe-Signature") as string;
+    const signature = req.headers.get("Stripe-Signature") as string;
 
     let event: Stripe.Event
 
