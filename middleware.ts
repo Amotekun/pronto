bf58abba0488045094eecf3f8a9c2901013adf23
Elijah Ajayi <elijahajayi444@gmail.com>
2024-05-02 22:52:46 -0700

    digest

--- middleware.ts
+++ middleware.ts
@@ -8,5 +8,10 @@ export default authMiddleware({
 });
  
 export const config = {
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+};
+
+/* export const config = {
   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
+ */
